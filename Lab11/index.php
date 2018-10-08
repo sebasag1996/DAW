cf6cf7f17a03a75cfc12d8a0cb5c277844256d28
sebasag1996 <marco.ag1996@gmail.com>
2018-10-08 17:41:31 +0000

    index.php lab11

--- Lab11/index.php
+++ Lab11/index.php
@@ -1,4 +1,13 @@
 <?php
+    echo $_POST["email"];
+    function check() {
+        if(isSet($_POST['email'] == '12345') && $_POST['password'] == '12345') { 
+            echo "correctas";   
+        } else { 
+            echo "Email i/o Password incorrectas ;) intentalo de nuevo";
+        }
+        
+    }
     include ("../partials/_header.html");
     require_once("util.php");
     include("_login.html");
